f902d8f73d18b3e15afcbb5c847b4891cf3e1d04
INNOMINDSHYD\jvadde <jvadde@innominds.com>
2024-05-05 15:21:14 +0530

    feat:add background color change with fixed array colors

---
+++ index.js
@@ -0,0 +1,26 @@
+const colors = [
+    "#FF0000",
+    "#00FF00",
+    "#0000FF",
+    "#FFFF00",
+    "#FF00FF",
+    "#00FFFF",
+    "#FFA500",
+    "#800080",
+    "#008000",
+    "#000080"
+];
+
+const btn = document.getElementById('btn')
+
+const getRandomNumber = (len) => {
+    return Math.floor(Math.random() * len)
+}
+const changeBackgroundColor = () => {
+    const randomNumber = getRandomNumber(colors.length)
+    const bgColor = colors[randomNumber]
+    document.body.style.backgroundColor = bgColor
+}
+btn.addEventListener('click', () => {
+    changeBackgroundColor()
+})  
